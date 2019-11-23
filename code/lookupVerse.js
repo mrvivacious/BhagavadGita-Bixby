b6e06a2d0d685436cf3ff327f35f14802ed29c84
Vivek Bhookya <v.bhookya98@gmail.com>
2019-11-23 07:41:50 -0600

    Gita imported, we can now query verses by ch-verse numbers

--- code/lookupVerse.js
+++ code/lookupVerse.js
@@ -1,5 +1,13 @@
+const BHAGAVAD_GITA = require('lib/fullGita').BHAGAVAD_GITA;
+
 module.exports.function = function lookupVerse(numChapter, numVerse) {
+  let sloka = BHAGAVAD_GITA[numChapter][numVerse]
+  let translation = sloka[1];
+  let verse = sloka[1];
+
   return {
+    SlokaSanskrit: sloka,
+    SlokaMeaning: translation,
     chapter: numChapter,
     verse: numVerse
   }
